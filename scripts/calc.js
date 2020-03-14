2e5d20c39b15a142b2216bb132fd05ff966f191b
brandonmongo <brandonmongo@hotmail.com>
2020-03-14 06:46:03 +0000

    Refactoring - Breaking Our Tests lesson completed

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,9 +1,14 @@
-function addition(numOne, numTwo) {
-    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
+Calculator = function() {
+this.value=0;
+};
 
-        return numOne + numTwo;
-    } else {
-        alert ("Error!");
+
+Calculator.prototype.add = function(number) {
+    if(typeof(number)== "number") {
+        this.value += number;
+    }
+    else {
+        alert("Error!");
     }
 }
 
